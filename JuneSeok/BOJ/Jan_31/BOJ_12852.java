2b3eeb66a14abd948534be668575816b228f565d
junseok-99 <angec5645@naver.com>
2024-01-31 22:18:25 +0900

    DP: BOJ_12852 / 1로 만들기 2 / 실버1

--- JuneSeok/BOJ/Jan_31/BOJ_12852.java
+++ JuneSeok/BOJ/Jan_31/BOJ_12852.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-
+    //jang
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
