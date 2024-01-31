03609d9d4d50872e7efcaa35a8550e8ad7ff64a6
seohyun kwon <seohyunkwon98@gmail.com>
2024-01-31 15:46:11 +0900

    DP: BOJ_2631 / 줄세우기 / 골드4

---
+++ seohyunkwon/BOJ/Jan_31/BOJ_2631.java
@@ -0,0 +1,32 @@
+package Jan_31;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_2631 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] children = new int[N];
+
+        for(int i=0; i<N; i++) {
+            children[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[N];
+        dp[0] = 1;
+
+        for(int i=1; i<children.length; i++) {
+            for(int j=0; j<i; j++) {
+                if(children[j] < children[i])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            if(dp[i]==0) dp[i] = 1;
+        }
+
+        System.out.println(N-Arrays.stream(dp).max().getAsInt());
+
+    }
+}
