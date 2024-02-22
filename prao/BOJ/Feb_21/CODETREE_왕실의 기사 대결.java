5dc973dc9e3c99c4a78ebfec72dec809e4aeb5a4
prao <101629085+dev-prao@users.noreply.github.com>
2024-02-22 15:54:17 +0900

    구현: CODETREE / 왕실의 기사 대결 / 골드3(수정)

--- prao/BOJ/Feb_21/CODETREE_왕실의 기사 대결.java
+++ prao/BOJ/Feb_21/CODETREE_왕실의 기사 대결.java
@@ -1,118 +1,142 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class 왕실의 기사 대결 {
 
-    static int L, N, Q;
-    static int sum;
-    static int[][][] board;
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-    static Knight[] knights;
+	static int L, N, Q;
+	static int[][] board;
+	static Knight[] knights;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        L = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        Q = Integer.parseInt(st.nextToken());
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
 
-        board = new int[L][L][N + 1];
-        knights = new Knight[N + 1];
-        for (int r = 0; r < L; r++) {
-            st = new StringTokenizer(br.readLine());
-            for (int c = 0; c < L; c++) {
-                board[r][c][0] = Integer.parseInt(st.nextToken());
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int row = Integer.parseInt(st.nextToken()) - 1;
-            int col = Integer.parseInt(st.nextToken()) - 1;
-            int height = Integer.parseInt(st.nextToken());
-            int width = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            knights[i] = new Knight(row, col, height, width, k, 0);
+		L = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		Q = Integer.parseInt(st.nextToken());
 
-            for (int r = row; r < row + height; r++) {
-                for (int c = col; c < col + width; c++) {
-                    board[r][c][i] = i;
-                }
-            }
-        }
+		board = new int[L + 1][L + 1];
+		knights = new Knight[N + 1];
+		int[] maxHp = new int[N + 1];
+		for (int i = 1; i <= L; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= L; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        for (int i = 0; i < Q; i++) {
-            st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            int dir = Integer.parseInt(st.nextToken());
-            move(num, dir);
-        }
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int h = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			knights[i] = new Knight(r, c, h, w, k);
+			maxHp[i] = knights[i].hp;
+		}
 
-        for (int i = 1; i <= N; i++) {
-            int count = 0;
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < N; k++) {
-                    if (board[j][k][i] > 0) {
-                        count++;
-                    }
-                }
-            }
-            if (knights[i].w * knights[i].h == count) {
-                sum += knights[i].dmg;
-            }
-        }
-        System.out.println(sum);
-    }
+		for (int command = 0; command < Q; command++) {
+			st = new StringTokenizer(br.readLine());
+			int knightIdx = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			if (knights[knightIdx].hp > 0)
+				move(knightIdx, d);
+		}
+		int res = 0;
+		for (int i = 1; i <= N; i++) {
+			if (knights[i].hp > 0)
+				res += maxHp[i] - knights[i].hp;
+		}
 
-    static void move(int index, int dir) {
-        Knight curr = knights[index];
-        for (int r = 0; r < L; r++) {
-            for (int c = 0; c < L; c++) {
-                if (board[r][c][index] == index) {
-                    int nr = r + dr[dir];
-                    int nc = c + dc[dir];
-                    if (nr < 0 || nc < 0 || nr + curr.h >= L || nc + curr.w >= L) {
-                        continue;
-                    }
-                    board[r][c][index] = 0;
-                    board[nr][nc][index] = index;
+		System.out.println(res);
 
-                    if (board[nr][nc][0] == 1) {
-                        curr.k--;
-                        curr.dmg++;
-                    }
+	}
 
-                    for (int i = 1; i <= N; i++) {
-                        if (i == index)
-                            continue;
-                        if (board[nr][nc][i] != 0) {
-                            move(i, dir);
-                        }
-                    }
-                }
-            }
-        }
-    }
+	static class Knight {
+		int row, col, height, width, hp, dmg;
+		boolean isUsed;
 
-    static class Knight {
-        int r;
-        int c;
-        int h;
-        int w;
-        int k;
-        int dmg;
+		public Knight(int row, int col, int height, int width, int hp) {
+			this.row = row;
+			this.col = col;
+			this.height = height;
+			this.width = width;
+			this.hp = hp;
+			dmg = 0;
+			isUsed = false;
+		}
 
-        public Knight(int r, int c, int h, int w, int k, int dmg) {
-            this.r = r;
-            this.c = c;
-            this.h = h;
-            this.w = w;
-            this.k = k;
-            this.dmg = dmg;
-        }
+	}
 
-    }
+	static void move(int knightIdx, int dir) {
+		if (canMove(knightIdx, dir)) {
+			for (int i = 1; i <= N; i++) {
+				if (knights[i].isUsed) {
+					knights[i].row += dr[dir];
+					knights[i].col += dc[dir];
+					knights[i].hp -= knights[i].dmg;
+				}
+				knights[i].isUsed = false;
+				knights[i].dmg = 0;
+			}
+		} else {
+			for (int i = 1; i <= N; i++) {
+				knights[i].isUsed = false;
+				knights[i].dmg = 0;
+			}
+		}
+	}
+
+	static boolean canMove(int knightIdx, int dir) {
+		Deque<Integer> queue = new ArrayDeque<>();
+
+		queue.add(knightIdx);
+		knights[knightIdx].isUsed = true;
+		while (!queue.isEmpty()) {
+			int idx = queue.poll();
+			int row = knights[idx].row + dr[dir];
+			int col = knights[idx].col + dc[dir];
+			int height = knights[idx].height;
+			int width = knights[idx].width;
+
+			if (!isValidRange(row, col, height, width))
+				return false;
+
+			for (int i = row; i < row + height; i++) {
+				for (int j = col; j < col + width; j++) {
+					if (board[i][j] == 1) {
+						knights[idx].dmg++;
+					} else if (board[i][j] == 2) {
+						return false;
+					}
+				}
+			}
+
+			for (int i = 1; i <= N; i++) {
+				if (knights[i].hp <= 0 || knights[i].isUsed || knights[i].row > row + height - 1
+					|| knights[i].row + knights[i].height - 1 < row
+					|| knights[i].col > col + width - 1
+					|| knights[i].col + knights[i].width - 1 < col) {
+					continue;
+				}
+				queue.add(i);
+				knights[i].isUsed = true;
+
+			}
+		}
+		knights[knightIdx].dmg = 0;
+		return true;
+	}
+
+	static boolean isValidRange(int row, int col, int height, int width) {
+		return row >= 1 && row + height - 1 <= L && col >= 1 && col + width - 1 <= L;
+	}
 }
